eaff4a8215ba3a1485672bcfe6c3c70e5b099e54
aryanchoudhary001 <aryanchoudhary0526@gmail.com>
2026-04-21 20:14:45 +0530

    2.py

---
+++ PHASE-3 LEVEL-4/2.py
@@ -0,0 +1,22 @@
+ #Print a square of stars recursively (n×n). 
+ def print_row(n):
+    # Base case for row
+    if n == 0:
+        return
+    print("*", end=" ")
+    print_row(n - 1)
+
+def print_square(n):
+    # Base case for square
+    if n == 0:
+        return
+    
+    # Print one row
+    print_row(n)
+    print()  # move to next line
+    
+    # Recursive call for remaining rows
+    print_square(n - 1)
+
+# Example
+print_square(5)
