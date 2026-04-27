13922ac0520d6d7136b27d2b598f1277203e29de
aryanchoudhary001 <aryanchoudhary0526@gmail.com>
2026-04-27 20:19:27 +0530

    updated

---
+++ PHASE-3 LEVEL-4/8.py
@@ -0,0 +1,13 @@
+# Input arrays
+A = [1, 2, 3, 4]
+B = [5, 6, 7, 8]
+
+# Empty result array
+C = []
+
+# Element-wise product
+for i in range(len(A)):
+    C.append(A[i] * B[i])
+
+# Output
+print("Resultant Array:", C)
