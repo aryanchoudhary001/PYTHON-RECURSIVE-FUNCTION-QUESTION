fb566a119d31dd8f59c182df2a54f25a869e5cd5
aryanchoudhary001 <aryanchoudhary0526@gmail.com>
2026-05-01 23:25:56 +0530

    1.py

--- PHASE-3 LEVEL-5/1.py
+++ PHASE-3 LEVEL-5/1.py
@@ -1,1 +1,10 @@
 #Check if the array is sorted in ascending order. 
+def is_sorted(arr):
+    for i in range(len(arr) - 1):
+        if arr[i] > arr[i + 1]:
+            return False
+    return True
+
+# Example
+arr = [1, 2, 3, 4, 5]
+print("Sorted" if is_sorted(arr) else "Not Sorted")
