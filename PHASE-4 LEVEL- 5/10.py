dd780d16fdb09eca6a2ea0a8907007f25fcaeede
aryanchoudhary001 <aryanchoudhary0526@gmail.com>
2026-07-06 16:07:47 +0530

    10

---
+++ PHASE-4 LEVEL- 5/10.py
@@ -0,0 +1,11 @@
+arr = list(map(int, input("Enter elements: ").split()))
+
+print("Unique elements:")
+
+for i in arr:
+    count = 0
+    for j in arr:
+        if i == j:
+            count += 1
+    if count == 1:
+        print(i, end=" ")
